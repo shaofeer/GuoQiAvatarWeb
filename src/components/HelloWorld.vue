de27867c6d9c178069056df3370267d13dcf1389
upuptop <pyfysf@163.com>
2019-09-29 11:26:29 +0800

    GuoQiAvatarWeb

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,155 @@
+<template>
+  <div id="home_div">
+    <div class="top_img_div">
+      <img :src="logo">
+      <img :src="topImg">
+    </div>
+
+    <mu-flex align-items="center">
+      <mu-flex justify-content="center" fill>
+        <mu-chip>
+          使用方式:上传头像之后,长按头像即可保存到手机。
+        </mu-chip>
+      </mu-flex>
+    </mu-flex>
+
+    <div id="carousel_div">
+
+
+      <mu-carousel
+        :cycle="false"
+        :hide-indicators="true"
+        :hide-controls="false"
+        @change="changeTemplate">
+
+        <mu-icon value="first_page" size="30" color="while" slot="left"></mu-icon>
+        <mu-icon value="last_page" size="30" color="while" slot="right"></mu-icon>
+
+        <mu-carousel-item>
+          <img class="item-img" :src="template1">
+        </mu-carousel-item>
+        <mu-carousel-item>
+          <img class="item-img" :src="template2">
+        </mu-carousel-item>
+        <mu-carousel-item>
+          <img class="item-img" :src="template3">
+        </mu-carousel-item>
+      </mu-carousel>
+    </div>
+
+
+    <mu-flex align-items="center">
+      <mu-flex justify-content="center" fill>
+        <mu-button @click="selectImg" color="primary">
+          <mu-icon left value="add_photo_alternate"></mu-icon>
+          上传头像
+        </mu-button>
+      </mu-flex>
+
+      <input ref="filElem" id="uploadFile" type="file" multiple @change='uploadFile($event)' style="display: none;"/>
+    </mu-flex>
+    <br>
+
+    <div class="happy_img_div">
+      <img :src="happyImg">
+      <img :src="logo">
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+  import template1 from '../assets/template_1.png'
+  import template2 from '../assets/template_2.png'
+  import template3 from '../assets/template_3.png'
+
+  import topImg from '../assets/topImg.png'
+  import happyImg from '../assets/happy.png'
+  import logo from '../assets/logo.png'
+
+  export default {
+    name: 'HelloWorld',
+    data () {
+      return {
+        template1,
+        template2,
+        template3,
+        topImg,
+        happyImg,
+        logo,
+      }
+    },
+    methods: {
+      changeTemplate: function (index) {
+        // this.$toast.error({'message': index, 'close': false, 'infoIcon': ''})
+      },
+      selectImg: function () {
+        this.$refs.filElem.dispatchEvent(new MouseEvent('click'))
+      },
+      uploadFile: function (event) {
+        let fileList = event.target.files
+        let file = fileList[0]
+
+        console.log(file)
+
+        let param = new FormData() //创建form对象
+        param.append('file', file)//通过append向form对象添加数据
+
+        let config = {
+          // headers: {'Content-Type': 'multipart/form-data'}
+        }
+
+        this.axios.post('/front/uploadFile', param, config).then(res => {
+          console.log(res)
+          this.template1 = res.data['template1']
+          this.template2 = res.data['template2']
+          this.template3 = res.data['template3']
+        }).catch(res => {
+          console.log(res)
+        })
+
+      },
+
+    },
+    watch: {
+      changeTemplate (n, o) {
+        console.log(n)
+      }
+    },
+    mounted () {
+    }
+  }
+</script>
+
+<style scoped>
+
+  #home_div {
+    background-image: url("../assets/backage.png");
+  }
+
+  .item-img {
+    width: 250px;
+    height: 250px;
+  }
+
+
+  .mu-carousel-item > img {
+    min-width: 0px;
+  }
+
+  .top_img_div, .happy_img_div {
+    text-align: center;
+  }
+
+  .top_img_div > img, .happy_img_div > img {
+    margin-top: 0px;
+    width: 300px;
+  }
+
+
+  .mu-carousel-item, #carousel_div, .mu-carousel {
+    height: 300px;
+  }
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,66 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+// import iView from 'iview';
+// import 'iview/dist/styles/iview.css';    // 使用 CSS
+
+// Vue.use(iView);
+
+import MuseUI from 'muse-ui'
+import 'muse-ui/dist/muse-ui.css'
+import Toast from 'muse-ui-toast'
+import './assets/font/material-icons.css'
+
+Vue.use(MuseUI)
+Vue.use(Toast)
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+// import Qs from 'qs'
+
+var axiosInstance = axios.create({
+  baseURL: 'http://api101.wintp.top/',
+  // baseURL: 'http://127.0.0.1:9009/',
+  // transformRequest: function (data) {
+  //   return Qs.stringify(data)
+  // },
+  // headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+})
+
+Vue.use(VueAxios, axiosInstance)
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: {App},
+  template: '<App/>'
+})
+
+/*
+     get
+        this.axios.get('/admin')
+          .then(res => {
+            console.log(res.data)
+          })
+          .catch(res => {
+            console.log(res.data)
+          })
+
+       post
+        this.axios.post('admin/login', {
+          'user_name': 'admin',
+          'user_pwd': 'admin'
+        }).then(res => {
+          //res.data do something right
+        }).catch(res => {
+          //do something wrong
+        })
+
+
+*/
