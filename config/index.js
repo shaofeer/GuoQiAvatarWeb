3846b51c0465a7e2de02a58af0b9a4ce21b21fcb
upuptop <pyfysf@163.com>
2019-12-20 18:12:13 +0800

    修改端口及地址

--- config/index.js
+++ config/index.js
@@ -9,13 +9,13 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/wechat/avatar',
+    assetsPublicPath: '/',
     proxyTable: {},
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
     // port: 80, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
-    port: 92939, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 8099, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
     notifyOnErrors: true,
@@ -51,7 +51,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/guoqingguoqi/',
+    assetsPublicPath: '/wechatavatar/',
     // assetsPublicPath: '/GuoQingJieAvatorWeb/',
 
     /**
